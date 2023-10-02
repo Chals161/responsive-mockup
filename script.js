17bea1ad028131fa43cc34d8575cf4186950f398
chala <chala@Sreyas-MBP.lan>
2023-10-02 15:09:31 -0400

    second commit

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function goToAboutPage() {
 function goWork() {
     window.location.href = "work.html";
 }
+
+const imageContainer = document.getElementById("image-container");
+
+imageContainer.addEventListener("scroll", () => {
+    const scrollTop = imageContainer.scrollTop;
+    const images = document.querySelectorAll("img");
+
+    images.forEach((img, index) => {
+        const imgTop = img.offsetTop;
+        const imgHeight = img.clientHeight;
+
+        if (scrollTop >= imgTop && scrollTop < imgTop + imgHeight) {
+            img.style.transform = `scale(${1 + (scrollTop - imgTop) / 100})`;
+        } else {
+            img.style.transform = "scale(1)";
+        }
+    });
+});
+
